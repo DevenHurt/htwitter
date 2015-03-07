d7dec1bf12854bea445519e0a10a66f94b641819
Deven Hurt <DevenHurt@gmail.com>
2015-03-07 17:52:38 -0500

    Added more sign in, sign out functionality (conditions for signed in/out)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307211918) do
+ActiveRecord::Schema.define(version: 20150307221701) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20150307211918) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
